ecda57e23ecb19c6ed17202167ff527afed8d0c7
HoldYourWaffle <ravivanrooijen@live.nl>
2019-10-01 00:06:41 +0200

    Initial version (bad error handling)

---
+++ index.js
@@ -0,0 +1,27 @@
+const Transform = require('stream').Transform;
+const PluginError = require('plugin-error');
+
+
+const PLUGIN_NAME = 'gulp-reshape';
+
+module.exports = function gulpReshape(reshapeCompiler) {
+	function transform(file, enc, callback) {
+		if (file.isNull()) {
+			return callback(null, file);
+		} else if (file.isBuffer()) {
+			reshapeCompiler.process(file.contents, { filename: file.path })
+				.then(result => {
+					file.contents = Buffer.from(result.output());
+					callback(null, file);
+				})
+				.catch(err => {
+					this.emit('error', new PluginError(PLUGIN_NAME, err));
+					//TODO fix potentially unhandled rejection error
+				});
+		} else if (file.isStream()) {
+			this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+		}
+	}
+	
+	return new Transform({ objectMode: true, transform });
+}
